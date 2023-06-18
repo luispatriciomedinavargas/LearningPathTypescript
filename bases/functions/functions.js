b916850f4e0e962036f832b4490157c3a190e528
patricio <p.medina.vargas@accenture.com>
2023-06-18 19:02:58 -0300

    basic functions and arrow functions

---
+++ bases/functions/functions.js
@@ -0,0 +1,22 @@
+"use strict";
+(() => {
+    const hero = "Flash";
+    //that is how you define a default funcrtion in js/ts
+    function returnName() {
+        return hero;
+    }
+    ;
+    //that is you define an arrow function
+    //ts knows that this function by default return a string, 
+    const arrowFunction1 = () => {
+        return "that is an arrow function";
+    };
+    //but also you can define that your arrow function return a string 
+    const arrowFunction = () => {
+        return "that is an arrow function";
+    };
+    //ts put the type of const as string
+    const heroName = returnName();
+    //but also you can define it
+    const heroName1 = returnName();
+})();
---
+++ bases/functions/functions.ts
@@ -0,0 +1,30 @@
+(() => {
+
+
+    const hero:string ="Flash";
+
+    //that is how you define a default funcrtion in js/ts
+    function returnName():string {
+        return hero;
+    };
+
+    //that is you define an arrow function
+    //ts knows that this function by default return a string, 
+    const arrowFunction1=()=>{
+        return "that is an arrow function";
+    }
+
+
+    //but also you can define that your arrow function return a string 
+    const arrowFunction=():string=>{
+        return "that is an arrow function";
+    }
+
+    //ts put the type of const as string
+    const heroName=returnName(); 
+
+
+      //but also you can define it
+      const heroName1:string=returnName(); 
+
+})()
