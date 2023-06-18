2f764630771b4841176de4f430cb5b399cbe0b39
patricio <p.medina.vargas@accenture.com>
2023-06-18 11:47:09 -0300

    tuples

---
+++ bases/tipos/tuples.ts
@@ -0,0 +1,18 @@
+(()=>{
+
+    //this is a tuple, not a array, so then, it awalys the frist value 
+    //will be string and the second will be a number
+const hero:[string, number] = ["Dr strange",100];
+
+
+    //here you can not assign a number to string
+    //here you can not assign a string to number
+    //hero[0] = 59
+    //hero[1]= "string"
+
+    hero[0] = "Thor"
+    hero[1] =  1000
+
+    console.log(hero);
+
+})()
