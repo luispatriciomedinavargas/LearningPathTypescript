7b640accb10c6989b0004fe8678e2a14dde7e2fe
patricio <p.medina.vargas@accenture.com>
2023-06-18 20:12:30 -0300

    added arguments as rest

---
+++ bases/functions/args-rest.ts
@@ -0,0 +1,13 @@
+(() => {
+
+
+    //you need only the frist params but sometines you need caught the rest, you can use the operator ... and
+    //assign a variable as the next example
+    const fullName = (fristName: string, ...restArgs: string[]): string => {
+        return `${fristName} ${restArgs.join(" ")}`;
+    }
+
+
+
+    const superman = fullName("clark", "joseph", "Kent");
+})()
