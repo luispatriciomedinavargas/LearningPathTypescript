35fafc46fb361fa6309aa11646a467803972a187
patricio <p.medina.vargas@accenture.com>
2023-06-20 20:19:50 -0300

    added type Hero

---
+++ bases/objects/types.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: "Barry Alen",
+        age: 24,
+        powers: [1, 2]
+    };
+    let superman = {
+        name: "Clark Kent",
+        age: 60,
+        powers: [1, 3]
+    };
+})();
