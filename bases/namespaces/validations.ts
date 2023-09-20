dcd031ad810d1288cc9a2be6828e21b7c1492a87
patricio <p.medina.vargas@accenture.com>
2023-09-20 20:09:01 -0300

    added an example about validation how works

---
+++ bases/namespaces/validations.ts
@@ -0,0 +1,10 @@
+namespace Validation{
+  export  const validateText = (text:string):boolean => {
+       
+        return (text.length > 3 ) ? true : false
+    }
+    const validateDate = (myDate:Date):boolean => {
+        return (isNaN(myDate.valueOf()))                                                                                                 
+
+    }
+  }
